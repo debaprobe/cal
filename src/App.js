99ebf6dd3d1ac20c1bd3db538172930a83a23d47
Debapriyo Bhowmick <134851035+debaprobe@users.noreply.github.com>
2023-08-02 09:38:52 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,61 @@
+
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [results,setResults]=  useState('')
+
+  const handleClick = (e) => {
+    setResults(results.concat(e.target.name))
+  }
+  const clear = () => {
+    setResults(' ')
+  }
+  const backspace = () => {
+    setResults(results.slice(0,- 1))
+  }
+
+  const calculate = () => {
+    try{
+
+      setResults(eval(results).toString())
+    }
+    catch(err){
+      setResults('error')
+    }
+  }
+  return (
+    <>
+    <div className='container'>
+      
+      <form>
+        <input type='text' value={results} />
+      </form>
+
+      <div className='keypad'>
+        <button  className= 'highlight' onClick={clear} id='clear'>Clear</button>
+        <button className= 'highlight' onClick={backspace} id='backspace'>C</button>
+        <button className= 'highlight' name='/' onClick={handleClick}>/</button>
+        <button name='7' onClick={handleClick}>7</button>
+        <button name='8' onClick={handleClick}>8</button>
+        <button name='9' onClick={handleClick}>9</button>
+        <button className= 'highlight' name='*' onClick={handleClick}>*</button>
+        <button name='4' onClick={handleClick}>4</button>
+        <button name='5' onClick={handleClick}>5</button>
+        <button name='6' onClick={handleClick}>6</button>
+        <button className= 'highlight' name='-' onClick={handleClick}>-</button>
+        <button name='1' onClick={handleClick}>1</button>
+        <button name='2' onClick={handleClick}>2</button>
+        <button name='3' onClick={handleClick}>3</button>
+        <button className= 'highlight' name='+' onClick={handleClick}>+</button>
+        <button name='0' onClick={handleClick}>0</button>
+        <button name='.' onClick={handleClick}>.</button>
+        <button className= 'highlight' onClick={calculate} id='results'>=</button>
+     
+      </div>
+      </div>
+    </>
+  ); 
+}
+
+export default App;
